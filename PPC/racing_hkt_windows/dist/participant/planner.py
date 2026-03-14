259db7b32696a4bfee9f34f3039bdbed881ef4b0
officialadhyayansaha-eng <official.adhyayansaha@gmail.com>
2026-03-14 23:36:59 +0530

    Update planner.py

--- PPC/racing_hkt_windows/dist/participant/planner.py
+++ PPC/racing_hkt_windows/dist/participant/planner.py
@@ -34,13 +34,25 @@ def plan(cones: list[dict]) -> list[dict]:
     yellow = np.array([[cone["x"], cone["y"]] for cone in cones if cone["side"] == "right"])
 
     # implement a planning algorithm to generate a path from the blue and yellow cones
+#   i=0
+#    x1,y1,x2,y2=0,0,0,0
+#    while i<len(blue):
+#        for cone in cones:
+#            if cone["index"]==i and cone["side"]=="left":
+#                x1=cone['x']
+#                y1=cone['y']
+#            if cone["index"]==i and cone["side"]=="right":
+#                x2=cone['x']
+#                y2=cone['y']
+#        path.append({"x":(x1+x2)/2 , "y"=(y1+y2)/2})
+#        i+=1
 
-
-
-
-
-
-
-
+    cones_left=[{"x":cone["x"], "y":cone["y"], "side":cone["side"] , "index":cone["index"]} for cone in cones if cone["side"]=="left"]
+    cones_right=[{"x":cone["x"], "y":cone["y"], "side":cone["side"] , "index":cone["index"]} for cone in cones if cone["side"]=="right"]
+    cones_left.sort(key=lambda cone: cone["index"])
+    cones_right.sort(key=lambda cone: cone["index"])
+    for i in range(min(len(cones_left),len(cones_right))):
+        path.append({"x":(cones_left[i]["x"]+cones_right[i]["x"])/2,"y":(cones_left[i]["y"]+cones_right[i]["y"])/2})
+    
+    
     return path
-
